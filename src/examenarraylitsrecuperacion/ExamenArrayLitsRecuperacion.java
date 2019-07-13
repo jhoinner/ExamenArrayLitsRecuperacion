cc208d3c36b1f928b96c9d5f055c661f4b65c425
jhoinner <jhoinnerballestas@gmail.com>
2019-07-12 22:13:07 -0500

    cuarto y ultimo commit examen java arraylist

--- src/examenarraylitsrecuperacion/ExamenArrayLitsRecuperacion.java
+++ src/examenarraylitsrecuperacion/ExamenArrayLitsRecuperacion.java
@@ -17,20 +17,20 @@ public class ExamenArrayLitsRecuperacion {
         ArrayList<Float> tiempo = new ArrayList<Float>();
         ArrayList<Float> velocidad = new ArrayList<Float>();
         int mejor=0,peor=0,op = 1,cont=0,i;
-        float vac=0;
+        float prom=0;
         
         while (op != 0){
             System.out.println("ingrese nombre del estudiante");
             nomb.add(l.next());
-            System.out.println("Ingrese la velocidad del estudiant");
+            System.out.println("Ingrese la velocidad del estudiante;  digite 0 para salir del ciclo");
             op = l.nextInt();
             if(op != 0){
                 velocidad.add((float)op);
                 tiempo.add(100 / velocidad.get(velocidad.size()-1));
-                vac = vac + tiempo.get(velocidad.size()-1);
+                prom = prom + tiempo.get(velocidad.size()-1);
             }
         }
-        vac = vac / tiempo.size();
+        prom = prom / tiempo.size();
         for(i=0; i<velocidad.size(); i++){
             if(velocidad.get(i)>velocidad.get(mejor)){
                 mejor = i;
@@ -38,11 +38,21 @@ public class ExamenArrayLitsRecuperacion {
             if(velocidad.get(i)<velocidad.get(peor)){
                 peor = i;
             }
-            if(tiempo.get(i)>vac){
+            if(tiempo.get(i)>prom){
                 cont=cont+1;
             }
             
+             System.out.println(nomb.get(i) + "\t" + velocidad.get(i) + "\t" + tiempo.get(i));
+            
         }
+        
+        System.out.println("el promedio es: " + prom);
+        System.out.println("el mas rapido es : " + nomb.get(mejor) + " con un tiempo de : " +tiempo.get(mejor));
+        System.out.println("el mas lento es :"+ nomb.get(peor) + " con un tiempo de : " + tiempo.get(peor));
+        System.out.println("el numero de estudiantes por encima del promedio es : " + cont );
+        
+        
+        
         
         
        
