703b3213278202f21f7b886ad3171f6ed3ebad1f
jhoinner <jhoinnerballestas@gmail.com>
2019-07-12 21:31:49 -0500

    segundo commit examen java arraylist

--- src/examenarraylitsrecuperacion/ExamenArrayLitsRecuperacion.java
+++ src/examenarraylitsrecuperacion/ExamenArrayLitsRecuperacion.java
@@ -1,21 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package examenarraylitsrecuperacion;
+
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  *
- * @author Usuario
+ * @author JhoinnerConstanteBallestas
  */
 public class ExamenArrayLitsRecuperacion {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner l = new Scanner(System.in);
+        ArrayList<String> nomb = new ArrayList<String>();
+        ArrayList<Float> tiempo = new ArrayList<Float>();
+        ArrayList<Float> velocidad = new ArrayList<Float>();
+        int mejor=0,op = 1;
+        float vac=0;
+        
+        while (op != 0){
+            System.out.println("ingrese nombre del estudiante");
+            nomb.add(l.next());
+            System.out.println("Ingrese la velocidad del estudiant");
+            op = l.nextInt();
+            if(op != 0){
+                velocidad.add((float)op);
+                tiempo.add(100 / velocidad.get(velocidad.size()-1));
+                vac = vac + tiempo.get(velocidad.size()-1);
+            }
+        }
+        vac = vac / tiempo.size();
+        
+       
+        
     }
     
 }
